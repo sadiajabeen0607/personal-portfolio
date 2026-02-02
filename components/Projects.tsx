36ea97ebf122229ec37d7dea79fe1ca64407f6a4
Sadia <sadiajabeen0607@gmail.com>
2026-02-02 10:32:10 +0500

    Fix select dropdown styles for dark and light mode

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -246,7 +246,7 @@ export default function Projects() {
             </a>
           </div>
         ) : (
-          <div className="absolute bottom-20 flex gap-4 z-30">
+          <div className="mt-8 flex gap-4 z-30">
             <p
               className="px-6 py-3 font-semibold rounded-lg bg-text"
             >
